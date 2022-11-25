626cbad4c01808a089f9c6ae490c7450cd66ec53
FUBAR2077 <gkiernozek@gmail.com>
2022-11-25 14:14:43 +0100

    3 files modified, 1 added

--- components/LoadingButton.js
+++ components/LoadingButton.js
@@ -1,5 +1,13 @@
 import React, { useState } from 'react';
-import './style.css';
+import '../src/style.css';
+
+// export default function LoadingButton(props) {
+//   return (
+//     <button onClick={props.onClick} type="button">
+//       {props.loading ? <div className="loader" /> : props.label}
+//     </button>
+//   );
+// }
 
 export const LoadingButton = (props) => {
   return (
--- src/App.js
+++ src/App.js
@@ -1,15 +1,30 @@
-import React from 'react';
+import React, { useState } from 'react';
+import LoadingButton from '../components/LoadingButton';
+import LoadingButton2 from '../components/LoadingButton';
 import './style.css';
-import LoadingButton from '../components/LoadingButton'
 
 export default function App() {
+  const [isLoading, setIsLoading] = useState(false);
+
   return (
     <>
       <div>
         <h1>Hello StackBlitz!</h1>
         <p>Start editing to see some magic happen :)</p>
+        <br />
       </div>
-      <LoadingButton />
+
+      <LoadingButton
+        label="press me"
+        loading={isLoading}
+        onClick={() => setIsLoading(!isLoading)}
+      />
+
+      <LoadingButton2
+        label="press me"
+        loading={isLoading}
+        onClick={() => setIsLoading(!isLoading)}
+      />
     </>
   );
 }
